2ba0c372a68584c685ebe26bfc60944e9d0d7c40
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-06 21:03:25 +0530

    Update docker login step

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $1
 systemctl start docker
-docker login -u $bamboo_dockeruser --password-stdin $bamboo_dockerpass 
+echo "$bamboo_dockerpass" | docker login --username $bamboo_dockeruser --password-stdin
 docker build . -t yashprime07/$1:latest
 docker push yashprime07/$1:latest
