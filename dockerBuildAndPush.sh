feaefd2cc2103224be0af4af8885912bf8728b72
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 12:17:06 +0530

    updating build script

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $1
 echo "$bamboo_dockerpass" | docker login --username "$bamboo_dockeruser" --password-stdin
-docker build . -t yashprime07/$1:$bamboo_planKey-$bamboo_shortPlanBranchName-$bamboo_buildNumber
-docker push yashprime07/$1:$bamboo_planKey-$bamboo_shortPlanBranchName-$bamboo_buildNumber
+docker build . -t yashprime07/$1:$bamboo_buildKey-$bamboo_buildNumber
+docker push yashprime07/$1:$bamboo_buildKey-$bamboo_buildNumber
