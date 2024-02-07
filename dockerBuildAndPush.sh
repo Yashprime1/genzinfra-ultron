d8dfe2aab627154829875e401f8f5f782c98e32c
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 11:54:22 +0530

    Adding dash and nb

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd $1
-systemctl start docker
 echo "$bamboo_dockerpass" | docker login --username "$bamboo_dockeruser" --password-stdin
-docker build . -t yashprime07/$1:latest
-docker push yashprime07/$1:latest
+docker build . -t yashprime07/$1:$bamboo_buildResultKey
+docker push yashprime07/$1:$bamboo_buildResultKey
