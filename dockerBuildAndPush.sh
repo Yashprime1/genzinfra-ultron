f94916b2c9bca57e939051bc0c47f60b5df4e866
yashdeep <yashdeep@clevertap.com>
2024-02-15 09:19:53 +0530

    Adding debug

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $1
+echo "$bamboo_dockeruser $bamboo_dockerpass"
 echo "$bamboo_dockerpass" | sudo docker login --username "$bamboo_dockeruser" --password-stdin
 sudo docker build . -t yashprime07/$1:$bamboo_planKey-$bamboo_buildNumber
 sudo docker push yashprime07/$1:$bamboo_planKey-$bamboo_buildNumber
